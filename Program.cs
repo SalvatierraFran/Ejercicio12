c1b27b15d9605788670b29bb9a9baaf90f0ba68c
SalvatierraFran <franco14_93@hotmail.com>
2016-03-28 22:35:20 -0300

    Programa que pide numeros y los suma hasta que el usuario lo solicite.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 12";
+
+            int numero, suma = 0;
+            char rta = 's';
+
+            do
+            {
+                Console.Write("Ingrese numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                suma = suma + numero;
+
+                Console.Write("\n¿Continua? S/N: ");
+                rta = char.Parse(Console.ReadLine());
+                ValidarRespuesta.ValidaS_N(rta);
+                ValidarRespuesta.NO(rta);
+
+            }while(rta == 's');
+
+            Console.Write("\nEl resultado de la suma es: " + suma);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ValidarRespuesta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio12
+{
+    class ValidarRespuesta
+    {
+        public static void ValidaS_N(char letra)
+        {
+            while (letra != 's' && letra != 'n')
+            {
+                Console.Write("Error. Reingrese: ");
+                letra = char.Parse(Console.ReadLine());
+            }
+        }
+
+        public static bool NO(char letra)
+        {
+            if (letra == 's')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
